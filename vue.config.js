34f4b2cfff8ae127ba89b8382d0211feaba41375
lining@yougu.tech <lining@yougu.tech>
2019-12-31 13:38:18 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -30,11 +30,8 @@ module.exports = {
     host: '0.0.0.0',
     proxy: {
       "/api/": {
-        target: "http://127.0.0.1:3000", //develop
-        // target: "http://192.168.1.193:7300/mock/5dc919286d973526997cad2b", // 模拟环境
-        // target: "http://192.168.8.16:18088", //li jie
-        // target: "http://192.168.1.194:18088", //li xin piao
-        // target: "http://192.168.1.150:18088", //yao zhipeng
+        target: "http://api.jmaogou.com", // develop
+        // target: "http://127.0.0.1:3000", // local
         ws: true,
         changeOrigin: true,
         pathRewrite: { "^/api/": "/" }
